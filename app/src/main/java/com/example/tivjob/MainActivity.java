bb7ab4ac4e609cf08cbd25eb5218855efdb41d86
EladBG-code <67457804+EladBG-code@users.noreply.github.com>
2020-06-26 10:54:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tivjob/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.tivjob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView WebsiteJ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebsiteJ = (WebView) findViewById(R.id.websiteID);
+        WebsiteJ.setWebViewClient(new WebViewClient());
+        WebsiteJ.loadUrl("https://bit.ly/2Y0xEuG");
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(WebsiteJ.canGoBack()){
+            WebsiteJ.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+}
